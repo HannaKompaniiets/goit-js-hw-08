ab8fae9bb117e8eea8c212791d590f920065f592
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-04-03 00:06:57 +0200

    1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below this linen
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsMarkup = createGallery(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </li> 
+        `;
+    })
+    .join("");
+}
+
+const gallery = new SimpleLightbox('.gallery__item a', {captionsData: 'alt', fadeSpee: '250'});
+
 
 console.log(galleryItems);

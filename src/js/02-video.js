9523431b89fec5df293de7803dcc74a690d4a95a
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-04-03 13:08:57 +0200

    2task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = "videoplayer-current-time";
+
+const onPlay = function(event){
+    localStorage.setItem(KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const videoTime = Number(localStorage.getItem('videoplayer-current-time')) || 0;
+
+player.setCurrentTime(videoTime).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        };
+    });
